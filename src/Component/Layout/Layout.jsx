b03e217e1b69d31dd0bd886d079ab387b4ad5043
sonchau1997 <chauson23041997@gmail.com>
2023-05-25 14:49:31 +0700

    List

--- src/Component/Layout/Layout.jsx
+++ src/Component/Layout/Layout.jsx
@@ -5,9 +5,10 @@ import SideShow from "Component/SlideShow";
 // import SideBar from "Component/SideBar";
 // import {SideBar} from "components/SideBar"
 
+
 const LayoutStyled = styled.div`
  background: #caeae6;
-  min-height: 120vh;
+  min-height: 1200vh;
   .right {
     float: right;
     width: 80%;
@@ -22,12 +23,38 @@ const LayoutStyled = styled.div`
     
   }
   .content-right{
+    display: flex;
     width: 40%;
     margin-right:50px;
+    flex-direction: column;
   }
-  .sideShow{
+  .top-creator{
+    //display: flex;
+    justify-content:space-around;
+    background-color: white;
+    width: 348px;
+    height: 478px;
+    margin-left:70px;
+    .title{
+width: 137px;
+height: 31px;
+
+
+font-family: 'DM Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 31px;
+font-feature-settings: 'salt' on;
+
+/* Text/color1 */
+
+color: #27262E;
+
+    }
   
   }
+
 `;
 
 const SideBarStyled = styled.div`
@@ -37,29 +64,61 @@ const SideBarStyled = styled.div`
 const ContentStyled = styled.div`
 background: #caeae6;
   margin-top: 64px;
-  height: 100vh;
+   height: 50vh; 
 `;
+const fakeData=[
+  {number:"1",id:"@jordan_",name:"Michael Jordan",avatar:"avatar"},
+  {number:"2",id:"@jordan_",name:"Michael Jordan",avatar:"avatar"}
+]
+const listPerson = fakeData.map((d) => <li key={d.number}>
+<div>
+ 
+  <div>
+  {d.number}
+  {d.avatar}
+  <div>
+    {d.name}</div>
+  <div>
+    {d.id}
+  </div>
+  </div>
+ 
+</div>
+  
+  
+  </li>);
 
 const PrimaryLayout = ({ children }) => {
   return (
     <LayoutStyled>
-      <SideBarStyled >  
+      <SideBarStyled >
         <SideBar></SideBar>
       </SideBarStyled>
 
       <div className="right">
         <Header />
         <div className="content">
-        <div className="content-left">
-        <SideShow className="sideShow"></SideShow>
+          <div className="content-left">
+            <SideShow className="sideShow"></SideShow>
+          </div>
+          <div className="content-right">
+            <ContentStyled>{children}</ContentStyled>
+            <div className="top-creator">
+              <div></div>
+              <p className="title">Top Creator</p>
+              <p>see more</p>
+              <div >
+                {listPerson}
+              </div>
+             
+         
+            </div>
+          
+          </div>
         </div>
-        <div className="content-right">
-        <ContentStyled>{children}</ContentStyled>
-        </div>
-        </div>
-       
-        
-        
+
+
+
       </div>
     </LayoutStyled>
   );
