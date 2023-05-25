a52b3170337c528f9be6130958dfb252b2e26957
sonchau1997 <chauson23041997@gmail.com>
2023-05-25 11:17:32 +0700

    fake data

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -4,6 +4,28 @@ import { styled } from "styled-components";
 const AppWrapper = styled.div`
   text-align: center;
 `;
+const fakeData = [
+  {
+    title: "Revenue",
+    amount: 5.00,
+    percent: 12.3,
+  },
+  {
+    title: "Spending",
+    amount: 2.00,
+    percent: 12.3,
+  },
+  {
+    title: "ROI",
+    amount: 14.2,
+    percent: -5.1,
+  },
+  {
+    title: "Estimated",
+    amount: 7.00,
+    percent: 3.2,
+  }
+]
 
  const Home=()=>{
     return(
@@ -14,10 +36,13 @@ const AppWrapper = styled.div`
       <header className="App-header">
     
         <div class="Card-container">
-        <Card title={"Revenue"} amount="5.00" percent={12.3}></Card> 
+        {
+              fakeData.map((item) => <Card title={item.title} amount={item.amount} percent={item.percent}></Card>)
+            }
+        {/* <Card title={"Revenue"} amount="5.00" percent={12.3}></Card> 
         <Card title={"Spending"} amount="2.00" percent={8.1}></Card> 
         <Card title={"Roi"} content="+14.02" percent={-5.1}></Card> 
-        <Card title={"Estimated"} amount="7.00" percent={3.2}></Card> 
+        <Card title={"Estimated"} amount="7.00" percent={3.2}></Card>  */}
         </div>
    
 
