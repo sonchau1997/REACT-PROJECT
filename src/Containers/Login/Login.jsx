3d1f581de9d24bcc90ec3b60d14319bb1e8ea7ac
sonchau1997 <chauson23041997@gmail.com>
2023-05-30 01:07:02 +0700

    Login

--- src/Containers/Login/Login.jsx
+++ src/Containers/Login/Login.jsx
@@ -3,60 +3,69 @@ import Arrow from "assets/arrow.png";
 import Google from "assets/Google.png";
 import Github from "assets/Github.png";
 import FaceBook from "assets/Facebook.png";
-import People from "assets/Character-working-laptop-sitting-chair.png"
-import Plant from "assets/cactus.png"
-import { Form } from "react-router-dom";
+import People from "assets/Character-working-laptop-sitting-chair.png";
+import Plant from "assets/cactus.png";
 
+import React from 'react';
+import { Form, Field } from 'react-final-form';
+import { FORM_ERROR } from 'final-form';
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-
-const StyleLogin = styled.div`
-
-.content {
-    display: flex;
-    margin: 264px 80px 100px 80px;
+const onSubmit = async values => {
+    await sleep(300)
+    if (values.username !== 'erikras') {
+        return { username: 'Unknown username' }
+    }
+    if (values.password !== 'finalformrocks') {
+        return { [FORM_ERROR]: 'Login Failed' }
+    }
+    window.alert('LOGIN SUCCESS!')
+}
+const SylesLogin = styled.div`
+.container{
+    margin: auto;
     width: 1920x;
     height: 1080px;
     background-color: aqua;
     background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);
-    
-}
-
+};
+.content {
+    display: flex;
+    margin: 264px 80px 100px 80px;
+    width: 1248px;
+    height: 579px;
+    background-color: white;
+    border-radius: 20px;
+};
 .left-content {
     display: flex;
     width: 70%;
-}
-
+};
 .login-form {
     display: flex;
     flex-direction: column;
     padding-top: 50px;
     padding-left: 50px;
-}
-
+    color: red;
+};
 .title {
     width: 166px;
     height: 84px;
-}
-input{
-    border-radius:12px;
-}
+};
 
-.password {
-    margin-top: 50px;
-}
+
 
 .flex-item {
     display: flex;
     justify-content: space-between;
-}
-
+};
 .login-form input {
     width: 478px;
     height: 46px;
     background-color: #8EC5FC;
-}
-
+    border-radius:12px;
+};
 .btn-login-with-icon {
     color: white;
     display: inline-flex;
@@ -71,50 +80,16 @@ input{
     height: 46px;
     margin-top: 50px;
     margin-left: 170px;
-}
-
-.left-content p {
-    text-align: center;
-    font-family: 'Nunito';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 19px;
-    color: #6096B4;
-}
-
+};
 .btn-login-with-icon img {
     width: 10px;
     height: 10px;
     margin-left: 10px;
-}
-
-.right-content {
-    display: flex;
-    background-color: #4295daa1;
-    width: 30%;
-    border-radius: 20px;
-    padding-right: 0;
-}
-
-.right-content .tree {
-    position: relative;
-    width: 198px;
-    height: 550px;
-    left: 150px;
-}
-
-.right-content .people {
-    position: relative;
-    width: 552px;
-    height: 542px;
-    right: 500px;
-}
-
+};
 .login {
     display: flex;
     justify-content: space-around;
-}
+};
 
 .google,
 .facebook,
@@ -126,119 +101,186 @@ input{
     background: #FFFFFF;
     border: 1px solid #6096B4;
     border-radius: 40px;
-}
+};
 
 .google,
 .facebook,
 .github img {
     padding-top: 10px;
-}
+};
+.left-content p {
+    text-align: center;
+    font-family: 'Nunito';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 19px;
+    color: #6096B4;
+};
+.right-content {
+    display: flex;
+    background-color: #4295daa1;
+    width: 30%;
+    border-radius: 20px;
+    padding-right: 0;
+};
+
+.right-content .tree {
+    position: relative;
+    width: 198px;
+    height: 550px;
+    left: 150px;
+};
+
+.right-content .people {
+    position: relative;
+    width: 552px;
+    height: 542px;
+    right: 500px;
+};
+
+
     
 `
 
-
-const Login = () => {
-    return (
-
-        <StyleLogin>
-
+const Login = () => (
+    <SylesLogin>
+        <div className="container">
             <div className="content">
                 <div className="left-content">
+                    <Form
+                        onSubmit={onSubmit}
+                        validate={values => {
+                            const errors = {}
+                            if (!values.username) {
+                                errors.username = 'Required'
+                            }
+                            if (!values.password) {
+                                errors.password = 'Required'
+                            }
+                            return errors
+                        }}
+                        render={({
+                            submitError,
+                            handleSubmit,
+                            form,
+                            submitting,
+                            pristine,
+                            values
+                        }) => (
+                            <form onSubmit={handleSubmit}>
 
+                                <div className="login-form">
+                                    <div className="title">
+                                        <h1>Login</h1>
+                                    </div>
+                                    <Field name="username">
+                                        {({ input, meta }) => (
+                                            <div className="username">
+                                                <label>Username</label>
+                                                <div>
+                                                    <input className="usernamein" {...input} type="text" placeholder="Username" />
+                                                    {(meta.error || meta.submitError) && meta.touched && (
+                                                        <span>{meta.error || meta.submitError}</span>
+                                                    )}
 
-                    <div className="login-form">
-                        <div class="title">
-                            <h1>Login</h1>
-                        </div>
-                        <Form>
-                        <form className="form">
-                            <div className="username">
-                                <label>Username</label>
+                                                </div>
+
+                                            </div>
+                                        )}
+                                    </Field>
+                                    <Field name="password">
+                                        {({ input, meta }) => (
+                                            <div className="password">
+                                                <label>Password</label>
+                                                <div>
+                                                    <input {...input} type="password" placeholder="Password" />
+                                                    {meta.error && meta.touched && <span>{meta.error}</span>}
+
+                                                </div>
+
+                                            </div>
+                                        )}
+                                    </Field>
+                                    {submitError && <div className="error">{submitError}</div>}
+                                    <div className="buttons">
+                                        <button className="btn-login-with-icon" type="submit" disabled={submitting}>
+                                            Login
+                                            <img src={Arrow} alt="" />
+                                        </button>
+                                        <button
+                                            type="button"
+                                            onClick={form.reset}
+                                            disabled={submitting || pristine}
+                                        >
+                                            Reset
+                                        </button>
+                                    </div>
+
+                                </div>
                                 <div>
-                                    <input class="usernamein" id="username" type="text" />
+                                    <p>or continute with</p>
                                 </div>
 
-                            </div>
-                            <div className="password">
-                                <div className="flex-item">
+                                <div className="login">
+                                    <div className="google">
+                                        <img src={Google} alt="google" />
 
-                                    <label>Password</label>
-                                    <span>Forgot Password</span>
+                                    </div>
+                                    <div className="github">
+                                        <img src={Github} alt="github" />
+                                    </div>
+                                    <div className="facebook">
+                                        <img src={FaceBook} alt="facebook" />
+                                    </div>
                                 </div>
-
                                 <div>
-                                    <input className="passwordin" id="password" type="password" />
+                                    <p>Don't have an account yet?<a href="#">Singup for free</a></p>
                                 </div>
 
-                            </div>
 
 
-                            <button onclick="login()" className="btn-login-with-icon" type="button">Login
-                                <img src={Arrow} alt="arrow" />
-                            </button>
-                        </form>
-                            
-                        </Form>
-                        
-                            
-                        
-                        
-
-                        <div>
-                            <p>or continute with</p>
-                        </div>
-                        <div className="login">
-                            <div className="google">
-                                <img src={Google} alt="google" />
-
-                            </div>
-                            <div className="github">
-                                <img src={Github} alt="github" />
-
-                            </div>
-                            <div className="facebook">
-                                <img src={FaceBook} alt="facebook" />
-
-
-                            </div>
-
-                        </div>
-                        <div>
-                            <p>Don't have an account yet?<span>Singup for free</span></p>
+                            </form>
 
 
 
 
-
-
-                        </div>
-
-
+                        )}
+                    />
 
 
 
-                    </div>
                 </div>
-                <div class="right-content">
+                <div className="right-content">
                     <img class="tree" src={Plant} alt="plant" />
                     <img class="people" src={People} alt="people" />
-
-
-
                 </div>
+
+
+
             </div>
 
-
-
-        </StyleLogin>
-
+        </div>
 
 
 
 
+    </SylesLogin>
 
 
 
-    )
-}
+
+
+)
+
 export default Login;
+
+
+
+
+
+
+
+
+
+
